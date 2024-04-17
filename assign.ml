41a60840b96489c3d9cd0cb7848877327696dd43
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-04-17 20:40:29 +0530

    reverse words using for loop

--- assign.ml
+++ assign.ml
@@ -17,18 +17,41 @@ let read_from_file filename =
     printf "Error: %s\n" msg;
     []
 
+(* This function is used to reverse a string *)
+let reverse_string str =
+  let len = String.length str in
+  let res = ref "" in
+  let curr = ref "" in
+  for i = 0 to len - 1 do
+    if str.[i] = ' ' then (
+      res := !curr ^ " " ^ !res; 
+      curr := "" 
+    )
+    else (
+      curr := !curr ^ String.make 1 str.[i] 
+    )
+  done;
+  !res ^ !curr 
+
+
+(* This function reverses the input string list
+let reverse_string_list lines = *)
+
+
 let () =
-  print_endline "Choose input method:";
-  print_endline "1. From file";
-  print_endline "2. From command line";
-  print_string "> ";
-  let choice = read_line () in
+  print_endline "Choos input method";
+  print_endline "1. For input from file";
+  print_endline "2. For input from command line";
+  print_endline "->";
+  let choice : string = read_line () in
   if choice = "1" then (
     print_endline "Enter the filename:";
     print_string "> ";
-    let filename = read_line () in
+    let filename : string = read_line () in
     let lines = read_from_file filename in
+    print_endline "The input text is:";
     List.iter print_endline lines
+    print_endline "The output text is:";
   )
   else if choice = "2" then (
     print_endline "Enter your text (press Enter twice to finish):";
@@ -38,8 +61,10 @@ let () =
       else read_lines (line :: acc)
     in
     let lines = read_lines [] in
+    print_endline "The input text is:";
     List.iter print_endline lines
+    print_endline "The output text is:";
   )
   else (
-    print_endline "Invalid choice"
+    print_endline "Invalid input"
   )
